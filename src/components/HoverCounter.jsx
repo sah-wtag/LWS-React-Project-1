c07d4746f1cb34833ad6bba2bb5c69d1491cc754
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-06-15 02:35:01 +0600

    React Render Props

--- src/components/HoverCounter.jsx
+++ src/components/HoverCounter.jsx
@@ -1,13 +1,7 @@
-import withCounter from "./HOC/withCounter";
-
-const HoverCounter = (props) => {
-  const { count, incrementCount } = props;
-
+export default function HoverCounter({ count, incrementCount }) {
   return (
     <div>
       <h1 onMouseOver={incrementCount}>Hovered {count} times</h1>
     </div>
-  ); 
-};
-
-export default withCounter(HoverCounter);
+  );
+}
