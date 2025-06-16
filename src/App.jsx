fc0474a967885ca0ac2b8b91e8f0bcefd0323635
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-06-17 01:42:34 +0600

    Context Api

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,43 @@ import Calculator from "./components/Calculator";
 import ClickCounter from "./components/ClickCounter";
 import Counter from "./components/Counter";
 import HoverCounter from "./components/HoverCounter";
+import Section from "./components/Section";
+import React from "react";
+import ThemeContext from "./contexts/themeContext";
 
-function App() {
-  return (
-    <div>
-      {/* <Form/> */}
-      {/* <Calculator /> */}
-      <Counter>
-        {(counter, incrementCount) => (
-          <ClickCounter count={counter} incrementCount={incrementCount} />
-        )}
-      </Counter>
-      <Counter>
-        {(counter, incrementCount) => (
-          <HoverCounter count={counter} incrementCount={incrementCount} />
-        )}
-      </Counter>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    theme: "light",
+    switchTheme: () => {
+      this.setState(({ theme }) => {
+        if (theme === "dark") {
+          return {
+            theme: "light",
+          };
+        }
+        return {
+          theme: "dark",
+        };
+      });
+    },
+  };
+  render() {
+    const { theme } = this.state;
+    return (
+      <div>
+        {/* <Form/> */}
+        {/* <Calculator /> */}
+        <Counter>
+          {(counter, incrementCount) => (
+            <ClickCounter count={counter} incrementCount={incrementCount} />
+          )}
+        </Counter>
+        <ThemeContext.Provider value={this.state}>
+          <Section />
+        </ThemeContext.Provider> 
+      </div>
+    );
+  }
 }
 
 export default App;
