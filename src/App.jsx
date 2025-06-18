ab58d193a6a23e8945200bb6b80573c73019608c
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-06-19 02:20:09 +0600

    React useCallback & useMemo hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,59 @@ import ClickCounter from "./components/ClickCounter";
 import Counter from "./components/Counter";
 import HoverCounter from "./components/HoverCounter";
 import Section from "./components/Section";
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import ThemeContext from "./contexts/themeContext";
 import Todo from "./components/Todo";
 import MyComponent from "./components/MyComponent";
+import Title from "./components/Title";
+import Button from "./components/Button";
+import ShowCount from "./components/ShowCount";
 
 function App() {
-  const [show, setShow] = useState(true);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const incrementByOne = useCallback(() => {
+    setCount1((prevCount) => prevCount + 1);
+  }, []);
+
+  const incrementByFive = useCallback(() => {
+    setCount2((prevCount) => prevCount + 5);
+  }, []);
+
+  const isEvenOrOdd = useMemo(() => {
+    let i = 0;
+    while (i < 1000000000) i += 1; // costly operation
+    return count1 % 2 === 0;
+  }, [count1]);
 
   return (
     <div className="app">
-      <div>{show && <MyComponent />}</div>
-      <p>
-        <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
-          {show ? "Hide post" : "Show post"}
-        </button>
-      </p>
+      <Title />
+      <ShowCount count={count1} title="Counter 1" />
+      <span>{isEvenOrOdd ? "Even" : "Odd"}</span>
+      <Button handleClick={incrementByOne}>Increment by one</Button>
+      <hr />
+      <ShowCount count={count2} title="Counter 2" />
+      <Button handleClick={incrementByFive}>Increment by five</Button>
     </div>
   );
 }
+
+// function App() {
+//   const [show, setShow] = useState(true);
+
+//   return (
+//     <div className="app">
+//       <div>{show && <MyComponent />}</div>
+//       <p>
+//         <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
+//           {show ? "Hide post" : "Show post"}
+//         </button>
+//       </p>
+//     </div>
+//   );
+// }
 
 // class App extends React.Component {
 //   state = {
@@ -46,7 +80,7 @@ function App() {
 //       <div>
 //         {/* <Form/> */}
 //         {/* <Calculator /> */}
-            // <Todo />
+// <Todo />
 //         <Counter>
 //           {(counter, incrementCount) => (
 //             <ClickCounter count={counter} incrementCount={incrementCount} />
