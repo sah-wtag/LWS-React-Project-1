d426715542ec02bdf5de89019386da2440fef5a1
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-06-18 13:06:16 +0600

    React useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,51 @@ import HoverCounter from "./components/HoverCounter";
 import Section from "./components/Section";
 import React from "react";
 import ThemeContext from "./contexts/themeContext";
+import Todo from "./components/Todo";
 
 class App extends React.Component {
-  state = {
-    theme: "light",
-    switchTheme: () => {
-      this.setState(({ theme }) => {
-        if (theme === "dark") {
-          return {
-            theme: "light",
-          };
-        }
-        return {
-          theme: "dark",
-        };
-      });
-    },
-  };
   render() {
-    const { theme } = this.state;
     return (
       <div>
-        {/* <Form/> */}
-        {/* <Calculator /> */}
-        <Counter>
-          {(counter, incrementCount) => (
-            <ClickCounter count={counter} incrementCount={incrementCount} />
-          )}
-        </Counter>
-        <ThemeContext.Provider value={this.state}>
-          <Section />
-        </ThemeContext.Provider> 
+        <Todo />
       </div>
     );
   }
 }
+
+// class App extends React.Component {
+//   state = {
+//     theme: "light",
+//     switchTheme: () => {
+//       this.setState(({ theme }) => {
+//         if (theme === "dark") {
+//           return {
+//             theme: "light",
+//           };
+//         }
+//         return {
+//           theme: "dark",
+//         };
+//       });
+//     },
+//   };
+//   render() {
+//     const { theme } = this.state;
+//     return (
+//       <div>
+//         {/* <Form/> */}
+//         {/* <Calculator /> */}
+//         <Counter>
+//           {(counter, incrementCount) => (
+//             <ClickCounter count={counter} incrementCount={incrementCount} />
+//           )}
+//         </Counter>
+//         <ThemeContext.Provider value={this.state}>
+//           <Section />
+//         </ThemeContext.Provider>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
